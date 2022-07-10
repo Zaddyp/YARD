04d298bc35dab3481784a4f27d44b852041aa9e6
Elijah Okoroh <okorohelijah@gmail.com>
2022-07-10 14:46:56 -0700

    users can login and signup

---
+++ app/src/main/java/com/example/yard/Activities/LoginActivity.java
@@ -0,0 +1,79 @@
+package com.example.yard.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.yard.R;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+  private TextView signUp;
+  private TextView tvEmailAddress;
+  private TextView password;
+  private Button logInButton;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+    if (ParseUser.getCurrentUser() != null) {
+      goMainActivity();
+    }
+    tvEmailAddress = findViewById(R.id.tvEmailAddress);
+    password = findViewById(R.id.etPassword);
+    signUp = findViewById(R.id.tvSign);
+    logInButton = findViewById(R.id.btnLogin);
+    signUp.setOnClickListener(
+        view -> {
+          Intent signUpIntent = new Intent(LoginActivity.this, SignupActivity.class);
+          startActivity(signUpIntent);
+        });
+    logInButton.setOnClickListener(
+        view -> {
+          String stringEmail = tvEmailAddress.getText().toString();
+          String stringPassword = password.getText().toString();
+          loginUser(stringEmail, stringPassword);
+        });
+  }
+
+  private void loginUser(String stringEmail, String stringPassword) {
+    ParseUser.logInInBackground(
+        stringEmail,
+        stringPassword,
+        (user, e) -> {
+          if (e != null) {
+            showAlert("Login failed", e.getMessage(), true);
+            return;
+          } else {
+            showAlert("Login Successful", "Your email is verified successfully", false);
+          }
+        });
+  }
+
+  private void goMainActivity() {
+    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+    startActivity(i);
+    finish();
+  }
+
+  private void showAlert(String title, String message, Boolean error) {
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    builder
+        .setTitle(title)
+        .setMessage(message)
+        .setPositiveButton(
+            "OK",
+            (dialogInterface, i) -> {
+              dialogInterface.dismiss();
+              if (!error) {
+                goMainActivity();
+              }
+            })
+        .show();
+  }
+}
