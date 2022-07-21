5b359ab85ae0dd6fed396c0f1b0b6db319a6a41f
Elijah Okoroh <okorohelijah@gmail.com>
2022-07-20 20:31:16 -0700

    Reformatted_Activitiess

--- app/src/main/java/com/example/yard/activities/PostActivity.java
+++ app/src/main/java/com/example/yard/activities/PostActivity.java
@@ -3,6 +3,7 @@ package com.example.yard.activities;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.Address;
@@ -30,7 +31,7 @@ import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import com.example.yard.R;
-import com.example.yard.adapter.PostCreation;
+import com.example.yard.adapter.Post;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.parse.ParseFile;
@@ -43,14 +44,13 @@ import java.util.Locale;
 
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class PostActivity extends AppCompatActivity
-    implements View.OnTouchListener,
-        GestureDetector.OnGestureListener,
-        GestureDetector.OnDoubleTapListener {
-  public static final String TAG = "PostActivity";
+    implements View.OnTouchListener, GestureDetector.OnGestureListener {
+  private static final String TAG = "PostActivity";
   private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 45;
+  private static final int GALLERY_REQUEST_CODE = 60;
   private static final int REQUEST_CODE = 100;
   private static final String PHOTO_FILE_NAME = "photo.jpg";
-  FusedLocationProviderClient fusedLocationProviderClient;
+  private FusedLocationProviderClient fusedLocationProviderClient;
   private TextView tvUserAddress;
   private TextView etDescription;
   private File photoFile;
@@ -61,9 +61,9 @@ public class PostActivity extends AppCompatActivity
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_post);
-    Button takePicture = findViewById(R.id.btnTakePicture);
-    Button submit = findViewById(R.id.btnSubmit);
-    Button upload = findViewById(R.id.btnUploadPicture);
+    Button btnTakePicture = findViewById(R.id.btnTakePicture);
+    Button btnSubmit = findViewById(R.id.btnSubmit);
+    Button btnUpload = findViewById(R.id.btnUploadPicture);
     etDescription = findViewById(R.id.tvDescription);
     ivImage = findViewById(R.id.ivImage);
     tvUserAddress = findViewById(R.id.tvLocationAddress);
@@ -72,24 +72,28 @@ public class PostActivity extends AppCompatActivity
     Button btnRemoveLocation = findViewById(R.id.btnRemoveLocation);
     fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
     btnGetLocation.setOnClickListener(view -> getLastLocation());
-    takePicture.setOnClickListener(view -> launchcamera());
+    btnTakePicture.setOnClickListener(view -> uploadImage(true));
     ivImage.setOnTouchListener(this);
     gestureDetector = new GestureDetector(this, this);
     Toast.makeText(
             this, " SWIPE left or right to DELETE a photo after taking it!", Toast.LENGTH_LONG)
         .show();
-    submit.setOnClickListener(
+    btnUpload.setOnClickListener(
         view -> {
-          String description = etDescription.getText().toString();
-          String userLocation = tvUserAddress.getText().toString();
+          uploadImage(false);
+        });
+    btnSubmit.setOnClickListener(
+        view -> {
+          String strDescription = etDescription.getText().toString();
+          String strUserLocation = tvUserAddress.getText().toString();
 
-          if (description.isEmpty()) {
+          if (strDescription.isEmpty()) {
             Toast.makeText(PostActivity.this, "Description cannot be empty", Toast.LENGTH_SHORT)
                 .show();
             return;
           }
           ParseUser currentUser = ParseUser.getCurrentUser();
-          savePost(description, userLocation, currentUser, photoFile);
+          savePost(strDescription, strUserLocation, currentUser, photoFile);
         });
     btnRemoveLocation.setOnClickListener(
         view -> {
@@ -145,14 +149,22 @@ public class PostActivity extends AppCompatActivity
     super.onRequestPermissionsResult(requestCode, permissions, grantResults);
   }
 
-  private void launchcamera() {
-    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-    photoFile = getPhotoFileUri(PHOTO_FILE_NAME);
-    Uri fileProvider =
-        FileProvider.getUriForFile(PostActivity.this, "com.codepath.fileprovider", photoFile);
-    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-    if (intent.resolveActivity(PostActivity.this.getPackageManager()) != null) {
-      startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+  private void uploadImage(Boolean takePicture) {
+    if (takePicture) {
+      Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+      photoFile = getPhotoFileUri(PHOTO_FILE_NAME);
+      Uri fileProvider =
+          FileProvider.getUriForFile(PostActivity.this, "com.codepath.fileprovider", photoFile);
+      intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+      if (intent.resolveActivity(PostActivity.this.getPackageManager()) != null) {
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+      }
+    } else {
+      Intent intent = new Intent(Intent.ACTION_PICK);
+      intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+      if (intent.resolveActivity(PostActivity.this.getPackageManager()) != null) {
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+      }
     }
   }
 
@@ -167,6 +179,24 @@ public class PostActivity extends AppCompatActivity
         Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
       }
     }
+    if (requestCode == GALLERY_REQUEST_CODE) {
+      if (resultCode == RESULT_OK) {
+        Uri fileUri = data.getData();
+        ivImage.setImageURI(fileUri);
+        photoFile = new File(getPaths(fileUri));
+      }
+    }
+  }
+
+  public String getPaths(Uri uri) {
+    String[] projection = {MediaStore.Images.Media.DATA};
+    Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+    if (cursor == null) return null;
+    int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+    cursor.moveToFirst();
+    String strResult = cursor.getString(column_index);
+    cursor.close();
+    return strResult;
   }
 
   public File getPhotoFileUri(String fileName) {
@@ -179,14 +209,14 @@ public class PostActivity extends AppCompatActivity
 
   private void savePost(
       String description, String userLocation, ParseUser currentUser, File photoFile) {
-    PostCreation postCreation = new PostCreation();
-    postCreation.setKeyDescription(description);
-    postCreation.setKeyLocation(userLocation);
+    Post post = new Post();
+    post.setKeyDescription(description);
+    post.setKeyLocation(userLocation);
     if (ivImage.getDrawable() != null) {
-      postCreation.setImage(new ParseFile(photoFile));
+      post.setImage(new ParseFile(photoFile));
     }
-    postCreation.setKeyUser(currentUser);
-    postCreation.saveInBackground(
+    post.setKeyUser(currentUser);
+    post.saveInBackground(
         e -> {
           if (e != null) {
             Log.e("error", "error while saving:" + e);
@@ -227,21 +257,6 @@ public class PostActivity extends AppCompatActivity
   @Override
   public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
     ivImage.setImageResource(0);
-    return false;
-  }
-
-  @Override
-  public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
-    return false;
-  }
-
-  @Override
-  public boolean onDoubleTap(MotionEvent motionEvent) {
-    return false;
-  }
-
-  @Override
-  public boolean onDoubleTapEvent(MotionEvent motionEvent) {
     return false;
   }
 }
